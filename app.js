70a662ca4e569454dda26ab18857cf1e2607a1bf
pedroLucasGt <110138836+pedroLucasGt@users.noreply.github.com>
2022-10-24 16:45:54 -0300

    commit teste

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se concetando ao banco de dados")
+console.log ("aplicação está se concetando ao banco de dados")
+//log de teste
+console.log ("commit do pedrinho")
